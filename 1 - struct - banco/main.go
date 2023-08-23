ae80273f3f7b839146dd53bf39a4a35fb3333499
Eduardo Diniz <eduardo.diniz987@gmail.com>
2023-08-22 23:37:16 -0300

    commit inicial

---
+++ 1 - struct - banco/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	titular := "Eduardo"
+	numeroAgencia := 589
+	numeroConta := 123456
+	saldo := 125.5
+
+	fmt.Println(titular, numeroAgencia, numeroConta, saldo)
+
+	titular2 := "Luciene"
+	numeroAgencia2 := 555
+	numeroConta2 := 654321
+	saldo2 := 200.0
+	fmt.Println(titular2, numeroAgencia2, numeroConta2, saldo2)
+}
