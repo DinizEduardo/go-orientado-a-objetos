a1a056bca8c688418d99b89c2c8f0244fab47699
Eduardo Diniz <eduardo.diniz987@gmail.com>
2023-08-23 00:26:56 -0300

    pacotes e visibilidade

---
+++ retornos-pacotes-visibilidade-banco/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"golang-poo/retornos-pacotes-visibilidade-banco/contas"
+)
+
+func main() {
+	contaDaSilvia := contas.ContaCorrente{Titular: "Silvia", Saldo: 300}
+	contaDoGustavo := contas.ContaCorrente{Titular: "Gustavo", Saldo: 100}
+
+	status := contaDoGustavo.Transferir(-200, &contaDaSilvia)
+	fmt.Println(contaDaSilvia)
+	fmt.Println(status)
+	fmt.Println(contaDoGustavo)
+}
